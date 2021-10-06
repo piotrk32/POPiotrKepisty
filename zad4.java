4f823b03a993da97d839a0ad9f3c02f4ef9475bd
piotrk32 <73549369+piotrk32@users.noreply.github.com>
2021-10-06 17:20:29 +0200

    1,2,3,4
    1,2,3,4

---
+++ zad4.java
@@ -0,0 +1,15 @@
+public class zad4 {
+
+    public static void main(String[] args) {
+
+        double a = 1000;
+        double b = a + a*0.06;
+        double c = b + b*0.06;
+        double suma = c;
+
+
+        System.out.println(suma);
+
+
+    }
+}
